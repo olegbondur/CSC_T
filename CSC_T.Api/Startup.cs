0d018b6ba4b49f1323333e8f671421fd26df03dd
olegbondur <olegbondur@gmail.com>
2018-03-22 21:27:32 +0200

    add swagger

--- CSC_T.Api/Startup.cs
+++ CSC_T.Api/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CSC_T.Api
 {
@@ -24,6 +26,25 @@ namespace CSC_T.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new Info { Title = "CSC Web API", Version = "v1", Description = "ASP.NET Core Web API" });
+               
+                swagger.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "SwaggerCSC.xml"));
+
+                swagger.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Name = "Authorization",
+                    In = "header",
+                    Description = "Please insert JWT with Bearer into field. Example: Bearer {token}",
+                    Type = "apiKey"
+                });
+                swagger.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                     {"Bearer", new string[] {} }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +54,12 @@ namespace CSC_T.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CSC Web API");
+            });
 
             app.UseMvc();
         }
